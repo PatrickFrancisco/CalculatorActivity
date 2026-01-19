4b77d4642666646aed9191be123962c9fa21d655
ch4irperson <ceit.francisco@gmail.com>
2026-01-19 22:28:59 +0800

    Added subtraction method

--- PracticeApp/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ PracticeApp/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -17,4 +17,7 @@ public class Calculator {
     public static void add (int x, int y){
         System.out.println(x+y);
     }
+    public static void sub (int x, int y){
+        System.out.println(x-y);
+    }
 }
